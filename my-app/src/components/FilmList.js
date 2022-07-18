bd7fcb0979af7d9d1e4cc0d30c8641abd90fecec
slahel <74063357+slahel@users.noreply.github.com>
2022-07-18 22:55:07 +0200

    css app

--- my-app/src/components/FilmList.js
+++ my-app/src/components/FilmList.js
@@ -3,9 +3,8 @@ import React from "react";
 export default function FilmList(props) {
   return (
     <div>
-      <h1>Films</h1>
       {props.films.map((film, index) => (
-        <div className="">
+        <div>
           <img src={film.Poster}></img>
         </div>
       ))}
