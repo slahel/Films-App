1b1b4402ef50c422dc03eb5c24c90285fbbfdf2c
slahel <74063357+slahel@users.noreply.github.com>
2022-07-22 12:08:07 +0200

    header component

--- my-app/src/components/FilmList.js
+++ my-app/src/components/FilmList.js
@@ -3,9 +3,8 @@ import React from "react";
 export default function FilmList(props) {
   return (
     <div>
-      <h1>Films</h1>
       {props.films.map((film, index) => (
-        <span className="poster col-6">
+        <span className="poster col-6" key={film.imdbID}>
           <img src={film.Poster} alt="poster"></img>
         </span>
       ))}
